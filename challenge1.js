e74884d9b535f249900bc4b3948e6a921e69b30e
ISABELLA MARTINEZ PRECIADO <isabella.martinez_p@uao.edu.co>
2023-08-02 17:08:36 -0500

    challenge1

---
+++ challenge1.js
@@ -0,0 +1,24 @@
+//Presentado por: Isabella Martinez Preciado
+//Codigo:2225525
+
+//Regular
+
+function evenorodd1 (n){
+        if (n % 2===0){
+            return("is even");
+        }else{
+            return("is odd");
+        }
+    }
+    
+    
+    //Arrow
+    const evenorodd1 = (n) => {
+        if (n % 2===0){
+            return("is even");
+        }else{
+            return("is odd");
+        }
+    }
+    
+    //diferencias 
