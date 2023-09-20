32049c8f36fe3c2386fde9ff8e317a2f0efadb9a
isabella202 <isabella.martinez_p@uao.edu.co>
2023-09-20 16:42:31 -0500

    Challenge 6
    Se realiza la tarea de crear un nuevo componente en react

---
+++ Challenge_6/src/FirstApp.js
@@ -0,0 +1,16 @@
+import './App.css';
+//import PropTypes from 'prop-types';
+
+const FirstApp = () => {
+  
+  return (
+    <> 
+    <h1>New Title</h1>
+    <span>10</span>
+    </>
+  )
+  }
+
+  
+  
+  export default FirstApp;
