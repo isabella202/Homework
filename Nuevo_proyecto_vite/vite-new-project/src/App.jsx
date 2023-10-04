f8a425d955ca139d4215b039820a1fa2540603a4
isabella202 <isabella.martinez_p@uao.edu.co>
2023-10-04 16:28:08 -0500

    Chalenge_9
    Se sube la tarea #9

---
+++ Nuevo_proyecto_vite/vite-new-project/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import { GiftGridExpertApp} from './components/GiftGridExpertApp'
+function App() {
+  return (
+    <div className="App">
+      <GiftGridExpertApp/>
+    </div>
+  );
+}
+
+export default App;
