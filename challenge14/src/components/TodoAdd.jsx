d2e0664233dff6d3387df22a556025f84b559b4c
isabella202 <isabella.martinez_p@uao.edu.co>
2023-10-11 18:45:12 -0500

    Challenge 14
    Se realizo el llamado de los 3 componentes desde el componente TodoApp

---
+++ challenge14/src/components/TodoAdd.jsx
@@ -0,0 +1,29 @@
+export const TodoAdd = ({ onNewTodo }) =>{
+
+    const onFormSubmit = (event) => {
+        event.preventDefault()
+
+        const newTodo = {
+            id: new Date().getTime(),
+            description: 'Hacer el proyecto',
+            done: false
+        }
+        onNewTodo(newTodo)
+    }
+
+    return (
+        <form onSubmit={(event) => onFormSubmit(event)}>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Tarea"
+            />
+            <button 
+                type="submit"
+                className="btn btn-outline-primary mt-1"
+            >
+                Agregar
+            </button>
+        </form>
+    )
+}
