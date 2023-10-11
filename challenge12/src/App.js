039beefb037dff96da348dc786b4e2ca9412311b
isabella202 <isabella.martinez_p@uao.edu.co>
2023-10-11 15:50:39 -0500

    Challenge 12
    Se realizaron los cambios para el challenge 12

---
+++ challenge12/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { MultipleCustomHooks } from './componentes/MultipleCustomHooks';
+
+function App() {
+  return (
+    <div className="App">
+      <MultipleCustomHooks />
+    </div>
+  );
+}
+
+export default App;
