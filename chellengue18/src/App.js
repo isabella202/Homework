5ff2be9bc4fa8c6fb398fa59c45fd016aa1d76d6
isabella202 <isabella.martinez_p@uao.edu.co>
2023-10-25 15:39:59 -0500

    Challenge 18
    Se creo el challenge18 el cual usamos Redux

---
+++ chellengue18/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {useDispatch, useSelector} from 'react-redux';
+import {increment, decrement} from './store/counterSlice'
+
+export const App = () => {
+  const {counter} = useSelector( state => state.counter);
+  const dispatch = useDispatch();
+
+
+  return (
+    <>
+    <h1>App</h1>
+    <hr />
+    <span>Counter is: {counter} </span>
+    <button
+      className='btn btn-primary'
+      onClick={ () => dispatch( increment() )}
+    > +1 </button>
+    <button
+      className='btn btn-primary'
+      onClick={ () => dispatch( decrement() )}
+    > -1 </button>
+    </>
+    
+  )
+
+}
+
+export default App;
